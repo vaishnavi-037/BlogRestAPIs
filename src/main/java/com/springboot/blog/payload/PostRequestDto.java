4399c64aa9e63a955de3b1a07565c67ef1f437d6
Vaishnavi Gupta <muskan.vgupta306@gmail.com>
2024-09-11 19:11:42 +0530

    Add Rest APIs description for Dto payload

--- src/main/java/com/springboot/blog/payload/PostRequestDto.java
+++ src/main/java/com/springboot/blog/payload/PostRequestDto.java
@@ -2,6 +2,7 @@ package com.springboot.blog.payload;
 
 import com.springboot.blog.entity.Category;
 import com.springboot.blog.entity.Post;
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.Size;
@@ -10,24 +11,39 @@ import lombok.Data;
 
 import java.util.HashSet;
 
+@Schema(
+        description = "Post Request Model Information"
+)
 @Data
 @AllArgsConstructor
 public class PostRequestDto {
 
+    @Schema(
+            description = "Blog Post Title"
+    )
     @NotEmpty
     @NotBlank
     @Size(min = 2, message = "Post tile should have at least 2 characters")
     private String title;
 
+    @Schema(
+            description = "Blog Post Description"
+    )
     @NotEmpty
     @NotBlank
     @Size(min = 10, message = "Post description should have at least 10 characters")
     private String description;
 
+    @Schema(
+            description = "Blog Post Content"
+    )
     @NotEmpty
     @NotBlank
     private String content;
 
+    @Schema(
+            description = "Blog Post Category Id"
+    )
     private Long categoryId;
 
     public Post toPostEntity(Category category) {
